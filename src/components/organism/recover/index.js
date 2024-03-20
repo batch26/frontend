dcb8afbe81702cec41970fc03ba2e4b8b587ce2c
Nasri <nasrisarif98@gmail.com>
2024-03-20 07:58:51 +0700

    add handesumbit recover

--- src/components/organism/recover/index.js
+++ src/components/organism/recover/index.js
@@ -1,26 +1,53 @@
-let index = () => {
+import { useState,useEffect } from "react";
+import axios from 'axios';
+// import useMessage from "../../../hooks/useMessage"
+
+let Index = () => {
+
+    // const [recover, setRecover] = useState([{}])
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
+    // const message = useMessage();
+    
+    const handleInput = () => {
+            axios.post("http://localhost:8080/api/account/forgot-password", {
+                email: name,
+                password: password
+        }).then((response) => {
+            // message.success(response)
+            console.log(response)
+            setPassword({ password });
+        }).catch((error) => {
+            console.log(error)
+        });
+        
+    }
+
     return (
         <form className="form-horizontal form-material" id="loginform" action="index.html">
             <h3 className="box-title m-b-20">Recover Password</h3>
             <div className="form-group">
                 <div className="col-xs-12">
                     <label>Username</label>
-                    <input className="form-control" type="text" required="true" placeholder="" />
+                    <input className="form-control" type="text" required={true} placeholder="" onChange={e => setName(e.target.value)}/>
                 </div>
             </div>
             <div className="form-group">
                 <div className="col-xs-12">
                     <label>Password</label>
-                    <input className="form-control" type="text" required="true" placeholder="" />
+                    <input className="form-control" type="text" required={true} placeholder="" onChange={e => setPassword(e.target.value)} />
                 </div>
             </div>
             <div className="form-group text-center m-t-20">
                 <div className="col-xs-12">
-                    <button className="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">Reset</button>
+                    <button className="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" type="submit"
+                    onClick={()=> handleInput()}>Reset</button>
+                    
                 </div>
             </div>
         </form>
     )
 }
 
-export default index;
+export default Index;
--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import Recover from "./components/page/recover";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Landing></Landing>
-    {/* <Recover></Recover> */}
+    {/* <Landing></Landing> */}
+    <Recover></Recover>
   </React.StrictMode>
 );
 
