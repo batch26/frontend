f904f2c972151d303c70853a12a08272e9758426
Luqmanhr25 <158405379+Luqmanhr25@users.noreply.github.com>
2024-03-22 00:45:04 +0700

    Add Modal

---
+++ src/components/organism/modal/demo/index.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { useState } from "react";
+import { Button, ButtonGroup, Form, Modal } from "react-bootstrap";
+import useMessage from "../../../hooks/useMessage";
+const ModalDemo = (props) => {
+    const [data, setData] = useState({
+        image: "",
+        label: ""
+    })
+    let msg = useMessage()
+    const Handelchange = (e) => {
+        const { name, value } = e.target
+
+        setData({ ...data, [name]: value })
+    }
+
+    const handlesubmit = () => {
+        axios.post("http://localhost:8080/api/demo", data).then((response) => {
+            msg.success(response)
+            props.listdemo()
+            setData({ image: "", label: "" })
+            props.setShowModal(false)
+        }).catch((error) => {
+            console.log(error.response)
+            msg.error(error)
+        })
+    }
+    return (
+        <Modal show={props.show} onHide={props.closeModal} size={"lg"} backdrop={"static"}> {/* Menggunakan onHide untuk menutup modal saat tombol close ditekan */}
+            <Modal.Header >
+                <Modal.Title>ADD DEMO</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form.Group className={"mb-3"}>
+                    <Form.Label>IMAGE URL</Form.Label>
+                    <Form.Control
+                        name={"image"}
+                        onChange={Handelchange}
+                        value={data.image}
+                    />
+                </Form.Group>
+                <Form.Group className={"mb-3"}>
+                    <Form.Label>LABEL</Form.Label>
+                    <Form.Control
+                        name={"label"}
+                        onChange={Handelchange}
+                        value={data.label}
+                    />
+                </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+                <ButtonGroup >
+                    <Button onClick={props.closeModal} style={{ marginRight: '10px' }}>CANCEL</Button>
+                    <Button onClick={handlesubmit}>SAVE</Button>
+                </ButtonGroup>
+
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default ModalDemo;
--- src/components/page/admin/demo/index.js
+++ src/components/page/admin/demo/index.js
@@ -1,24 +1,56 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-
+import "../../../../static/admin/css/style.css";
+import "../../../../static/admin/assets/plugins/bootstrap/css/bootstrap.min.css";
+import ModalDemo from "../../../organism/modal/demo";
+import useMessage from "../../../hooks/useMessage";
 const Demo = () => {
 
     const [demo, setDemo] = useState([{}])
 
-    useEffect(() => {
+    const [showModal, setShowModal] = useState(false);
+
+    let msg = useMessage();
+
+    const listdemo = () => {
         axios.get("http://localhost:8080/api/demos")
             .then((response) => {
+                console.log(response.data)
                 setDemo(response.data.data);
             })
             .catch((error) => {
-                console.log(error);
+
+                console.log(error.response);
             });
+    }
+    
+    useEffect(() => {
+        listdemo()
     }, []);
 
+    const openModal = () => {
+        setShowModal(true);
+    };
+
+    const closeModal = () => {
+        setShowModal(false);
+    };
+
+    const deleteById = (id) => {
+        console.log(id)
+        axios.delete(`http://localhost:8080/api/demo/${id}`)
+            .then((response) => {
+                console.log(response)
+                msg.success(response)
+            }).catch((error) => {
+                console.log(error)
+            })
+    }
+
     return (
-        <div class="container">
+        <div className="container">
             <h1>Demo</h1>
-            <button className="btn btn-primary" onclick="show()">CREATE</button>
+            <button className="btn btn-primary" onClick={openModal}>CREATE</button>
             <table id="myTable" className="table table-success table-striped" style={{ marginTop: '10px' }}>
                 <thead>
                     <tr>
@@ -29,20 +61,21 @@ const Demo = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {demo.map((data,index) => (
+                    {demo.map((data, index) => (
                         <tr key={index}>
-                            <td>{data.id}</td>
+                            <td>{index + 1}</td>
                             <td>{data.image}</td>
                             <td>{data.label}</td>
                             <td>
-                            <button className="btn btn-warning" onclick="show()">EDIT</button>
-                            <span style={{ marginRight: '5px' }}></span> 
-                            <button className="btn btn-danger" onclick="show()">DELETE</button>
+                                <button className="btn btn-warning" onClick={() => console.log("Edit clicked")}>EDIT</button>
+                                <span style={{ marginRight: '5px' }}></span>
+                                <button className="btn btn-danger" onClick={() => deleteById(data.id)}>DELETE</button>
                             </td>
                         </tr>
                     ))}
                 </tbody>
             </table>
+            <ModalDemo show={showModal} closeModal={closeModal} listdemo={listdemo} showModal={setShowModal}/>
         </div>
     )
 }
