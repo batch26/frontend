375570c7655f4daeb98abba85a329dc9c6d0951f
Luqmanhr25 <158405379+Luqmanhr25@users.noreply.github.com>
2024-03-21 21:19:18 +0700

    Show Data Demo

--- src/components/page/admin/demo/index.js
+++ src/components/page/admin/demo/index.js
@@ -1,6 +1,49 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Demo = () => {
+
+    const [demo, setDemo] = useState([{}])
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/api/demos")
+            .then((response) => {
+                setDemo(response.data.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
     return (
-        <h1>Demo</h1>
+        <div class="container">
+            <h1>Demo</h1>
+            <button className="btn btn-primary" onclick="show()">CREATE</button>
+            <table id="myTable" className="table table-success table-striped" style={{ marginTop: '10px' }}>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>IMAGE</th>
+                        <th>LABEL</th>
+                        <th>ACTION</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {demo.map((data,index) => (
+                        <tr key={index}>
+                            <td>{data.id}</td>
+                            <td>{data.image}</td>
+                            <td>{data.label}</td>
+                            <td>
+                            <button className="btn btn-warning" onclick="show()">EDIT</button>
+                            <span style={{ marginRight: '5px' }}></span> 
+                            <button className="btn btn-danger" onclick="show()">DELETE</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
